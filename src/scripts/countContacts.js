5b465da7628bbf1030aeaac0316aa25ee9e4d6c7
Your Name <e.scaroline02@gmail.com>
2024-07-28 22:25:49 +0300

    count fc

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  let parsedArr = [];
+  try {
+    const arr = await fs.readFile(PATH_DB);
+    parsedArr = arr && JSON.parse(arr);
+    return parsedArr.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await countContacts());
