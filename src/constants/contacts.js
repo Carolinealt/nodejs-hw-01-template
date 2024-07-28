830210b8307f699f5ddbe983242d818b5761ada8
Your Name <e.scaroline02@gmail.com>
2024-07-28 22:18:34 +0300

    add-one fc

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,3 +1,2 @@
 import path from 'node:path';
-const FILE_PATH = path.resolve('src','db', 'db.json');
-export const PATH_DB = FILE_PATH;;
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const addOneContact = async () => {};
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+export const addOneContact = async () => {
+  let parsedArr = [];
+  try {
+    const arrayContacts = await fs.readFile(PATH_DB);
+    parsedArr = arrayContacts && JSON.parse(arrayContacts);
+    const user = createFakeContact();
+    parsedArr.push(user);
+    await fs.writeFile(PATH_DB, JSON.stringify(parsedArr));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 addOneContact();
