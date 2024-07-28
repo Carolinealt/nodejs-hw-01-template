7a16eb4ed7f81740064fdcae1701b3384e0158d1
Your Name <e.scaroline02@gmail.com>
2024-07-28 22:39:00 +0300

    removeLastContact fc

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  let parsedArr = [];
+  try {
+    const arrContacts = await fs.readFile(PATH_DB);
+    parsedArr = arrContacts && JSON.parse(arrContacts);
+    parsedArr.pop();
+    await fs.writeFile(PATH_DB, JSON.stringify(parsedArr));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
